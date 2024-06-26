e809de0c00f2b9307e4db93cabbe107420c41e87
Nbh <87258864+ByeongHuiNa@users.noreply.github.com>
2024-06-26 18:03:10 +0900

    [Bronze III] Title: 최소, 최대, Time: 1744 ms, Memory: 318852 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt();
+		
+
+		int arr[] = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+		
+		int max = arr[0], min = arr[0];
+		
+		for (int i = 1; i < N; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		System.out.print(min + " " + max);
+	}
+}
