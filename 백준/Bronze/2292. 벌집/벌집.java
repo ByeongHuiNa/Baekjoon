f2f3a485ab27b46e2141e2ae6c3c91f7c24fbf66
나병희 <87258864+ByeongHuiNa@users.noreply.github.com>
2024-07-11 20:21:37 +0900

    [Bronze II] Title: 벌집, Time: 180 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		
+		int cnt = 1;
+		int ran = 2;
+
+		if (N == 1) {
+			System.out.println(1);
+		} else {
+			while (ran <= N) {
+				ran = ran + (6 * cnt);
+				cnt++;
+			}
+			System.out.println(cnt);
+		}
+
+	}
+}
